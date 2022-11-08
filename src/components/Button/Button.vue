cb2ad8816e37fa31c4985e1ebea367084783d19a
Сергей Вилков <sergey.vilkov@rsv.ru>
2022-11-08 19:27:20 +0300

    Init

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,42 @@
+<template>
+    <button
+        class="button"
+        @click="onClick"
+    >
+        {{ label }}
+    </button>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
+import type { Props } from './types/props';
+
+const emit = defineEmits(['click']);
+
+defineProps<Props>();
+
+function onClick () {
+    emit('click');
+}
+
+</script>
+
+<style lang="scss" scoped>
+.button {
+    outline: none;
+    border: none;
+    padding: 16px;
+    font-size: 16px;
+    background: #0084ff;
+    color: #fff;
+    transition: all .3s ease-in-out;
+
+    @media (min-width: 1024px) {
+        cursor: pointer;
+
+        &:hover {
+            background: #2c99ff;
+        }
+    }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="main">
+        <Button
+            label="Show auth form"
+            @click="showForm"
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import Button from '@/components/Button/Button.vue';
+
+const isShowForm = ref<boolean>(false);
+
+function showForm() {
+    console.log('showForm');
+    isShowForm.value = true;
+}
+</script>
